40551f316c9f01416c635f3edce673ddc7321a36
rustmaestro <81419749+rustmaestro@users.noreply.github.com>
2024-02-23 16:49:40 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+Blanks out the classic Magento 2 frontend, without affecting APIs that are implemented in the frontend area (eg. Adyen)
---
+++ src/Plugin/CancelLayoutRendering.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace ReachDigital\BlankClassicFrontend\Plugin;
+
+use Magento\Framework\App\Area;
+use Magento\Framework\App\Config\ScopeConfigInterface;
+use Magento\Framework\App\State;
+
+class CancelLayoutRendering
+{
+    /**
+     * @var State
+     */
+    protected $_state;
+
+    /**
+     * @var ScopeConfigInterface
+     */
+    protected $_scopeConfig;
+
+    /**
+     * @param State $state
+     */
+    public function __construct (
+        State $state,
+        ScopeConfigInterface $scopeConfig
+    ) {
+        $this->_state = $state;
+        $this->_scopeConfig = $scopeConfig;
+    }
+
+    public function afterGetOutput($subject, $result) {
+        if ($this->_state->getAreaCode() == Area::AREA_FRONTEND) {
+            if($this->_scopeConfig->isSetFlag("blank_classic_frontend/graphql_mesh/should_redirect")) {
+                $base_link_url = $this->_scopeConfig->getValue('web/secure/base_link_url');
+                return sprintf('<meta http-equiv="refresh" content="0;url=%s">', $base_link_url);
+            } else {
+                return '';
+            }
+        }
+
+        return $result;
+    }
+}
